6be55325b07267da632483b56552c173b920ffda
AronEfr <ketasely@gmail.com>
2024-06-07 12:19:06 +0300

    Day 2 morning session

--- Day 2 Morning session/efrata_aron_day2.py
+++ Day 2 Morning session/efrata_aron_day2.py
@@ -15,32 +15,37 @@ comprehensions)
 
 # if, elif, else
 
-# age = 70
+#Example 1
+print("Example 1")
+Age = 70
 
-# if age < 18: 
-#     print('You are an adult')
-# elif age > 65:
-#     print('you are senior citizen')
+if Age < 18: 
+    print('You are an adult')
+elif Age > 65:
+    print('you are senior citizen')
     
-# else:
-#     print('You are a Youth')
+else:
+    print('You are a Youth')
     
     
+    
+  # Example 2 
+print("Example 2") 
 # 90, above above is Excellent, Equal 80 and above is Very good, if 70, good
 # otherwise , not good. 
 
-# Example 2
-# grade = 50
 
-# if grade >=90:
-#     print('Excellent')
-# elif grade >= 80:
-#     print('very good')
-# elif grade >= 70:
-#     print('Good')
+grade = 50
 
-# else: 
-#     print('Not good')
+if grade >=90:
+    print('Excellent')
+elif grade >= 80:
+    print('very good')
+elif grade >= 70:
+    print('Good')
+
+else: 
+    print('Not good')
 
 """ 
  Exercise:  Scenario: Write a python script to determine the price 
@@ -53,24 +58,29 @@ comprehensions)
     """
     
     #Exercise 1 solution
-    
-# age = int(input("whats your age"))
-# fullprice= 2000
-# if age <13:
-#     discount= 1000
-#     Price= fullprice-discount
-#     print("Children get discount of shs 1000,")
-#     print("pay " , Price)
+print()   
+print("Exercise Solution____") 
+
+age = int(input("whats your age"))
+fullprice= 2000
+if age <13:
+    discount= 1000
+    Price= fullprice-discount
+    print("Children pay " , Price)
    
     
-# elif 13 < age < 18:
-#      print("Teenagers get discount of shs 500")
+elif 13 < age < 18:
+     discount= 1000
+     Price= fullprice-discount
+     print("Teenagers pay " , Price)
      
-# elif 65 > age > 18:
-#          print("Adults pay shs 2000")
+elif 65 > age > 18:
+     discount= 1000
+     Price= fullprice-discount
+     print("Adults pay " , Price)
          
-# else:
-#     print("senior citizens pay shs 5000")
+else:
+     print("senior citizens pay shs 5000")
     
     
 # Loops (for, while)
@@ -82,18 +92,21 @@ comprehensions)
     """
     
 #Example 3
+print()   
+print("Example 3")   
+cars = ['Tesla', 'Audi', 'BMW', 'Jeep', 'RangeRover']
+  
     
-# cars = ['Tesla', 'Audi', 'BMW', 'Jeep', 'RangeRover']
-    
-# for car in cars:
-#         print(car)
-    
-#Example 4  count 1 to 10
+for car in cars:
+        print(car)
+print()    
+# Example 4  count 1 to 10
+print("Example 4")
  
-# count = 1
-# while count <= 10:
-#      print("Count 1 to 10:", count)
-#      count += 1
+count = 1
+while count <= 10:
+     print("Count 1 to 10:", count)
+     count += 1
     
 
 #Exercise 2
@@ -103,14 +116,26 @@ comprehensions)
     Create your own list of favorite colors using for loop
     2.  Create a reverse of the input 12345 to be 54321 using while loop
     """
+print()
+print("Exercise 2 solution_______________________________________________________________")
     
+    #exercise solution
 colors=["red", "black", "lavender", "turquoise"]
 for color in colors:
-    print(color) 
     
+    print(color) 
+print()
+ 
+print("reversecolors")   
+reversed_colors=colors[::-1]
+
+for color in reversed_colors:
+    print(color)
+print()    
+
     #using while loop to reverse
     
-    counter = 5
+counter = 5
 while counter >=1:
     
      print("Count 5 to 1:", counter)
