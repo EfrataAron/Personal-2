debe6456366aff73977291ff55766dacb6e4cbe4
Efrata Aron <126228991+EfrataAron@users.noreply.github.com>
2024-06-07 11:38:30 +0300

    day 1 morning

---
+++ Day 1 morning/syntax_and_comment.py
@@ -0,0 +1,12 @@
+#EFRATA ARON
+#REG NO 22/X/5249/PS
+#STUDENT NO 2200705249
+
+#SYNTAX AND COMMENTS ASSIGNMENT
+
+print("EFRATA ARON")
+
+First_name = input("what is your first name?")
+Favourite_color = input("what is your favourite color?")
+
+print(First_name + " " + "prefers" + " " + Favourite_color + " " + "to any other colour")
